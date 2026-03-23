849e01c04e5e75be3680852a4b3f142a7abdf0cc
Deasta <Danveast@gmail.com>
2026-03-23 14:29:46 +1100

    add webhook example

---
+++ Task2.1WebHook/webhook_temp_light.ino
@@ -0,0 +1,102 @@
+/*
+  WriteMultipleFields
+  
+  Description: Writes values to fields 1,2,3,4 and status in a single ThingSpeak update every 20 seconds.
+  
+  Hardware: Arduino MKR WiFi 1010
+  
+  !!! IMPORTANT - Modify the secrets.h file for this project with your network connection and ThingSpeak channel details. !!!
+  
+  Note:
+  - Requires WiFiNINA library
+  - This example is written for a network using WPA encryption. For WEP or WPA, change the WiFi.begin() call accordingly.
+  
+  ThingSpeak ( https://www.thingspeak.com ) is an analytic IoT platform service that allows you to aggregate, visualize, and 
+  analyze live data streams in the cloud. Visit https://www.thingspeak.com to sign up for a free account and create a channel.  
+  
+  Documentation for the ThingSpeak Communication Library for Arduino is in the README.md folder where the library was installed.
+  See https://www.mathworks.com/help/thingspeak/index.html for the full ThingSpeak documentation.
+  
+  For licensing information, see the accompanying license file.
+  
+  Copyright 2020, The MathWorks, Inc.
+*/
+
+#include <WiFiNINA.h>
+#include "secrets.h"
+#include "ThingSpeak.h" // always include thingspeak header file after other header files and custom macros
+#include "DHT.h"
+//Setting GHT pin and type
+#define DHTPIN 12 
+#define DHTTYPE DHT22
+#define BRIGHTPIN A1
+DHT dht(DHTPIN, DHTTYPE);
+//Set light pin
+
+
+char ssid[] = SECRET_SSID;   // your network SSID (name) 
+char pass[] = SECRET_PASS;   // your network password
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+WiFiClient  client;
+
+unsigned long myChannelNumber = SECRET_CH_ID;
+const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
+
+
+void setup() {
+  Serial.begin(115200);  // Initialize serial
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo native USB port only
+  }
+  
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+  pinMode(BRIGHTPIN, INPUT);
+  ThingSpeak.begin(client);  //Initialize ThingSpeak
+  dht.begin(); //Initialise DHT
+}
+
+void loop() {
+
+  // Connect or reconnect to WiFi
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(SECRET_SSID);
+    while(WiFi.status() != WL_CONNECTED){
+      WiFi.begin(ssid, pass); // Connect to WPA/WPA2 network. Change this line if using open or WEP network
+      Serial.print(".");
+      delay(5000);     
+    } 
+    Serial.println("\nConnected.");
+  }
+  int brightness = analogRead(BRIGHTPIN);
+  float temperature = dht.readTemperature();
+    if (isnan(temperature)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+  Serial.print(F(" Temperature: "));
+  Serial.print(temperature);
+  Serial.print("  Brightness: ");
+  Serial.println(brightness);
+
+  // set the fields with the values
+  ThingSpeak.setField(1, temperature);
+  ThingSpeak.setField(2, brightness);
+
+  // write to the ThingSpeak channel
+  int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  if(x == 200){
+    Serial.println("Channel update successful.");
+  }
+  else{
+    Serial.println("Problem updating channel. HTTP error code " + String(x));
+  }
+  
+  
+  delay(20000); // Wait 20 seconds to update the channel again
+}
